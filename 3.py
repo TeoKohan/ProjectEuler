5562f3df418c78b67d6e91f6fd0376d956b1d86f
TK <teo.kohan@gmail.com>
2023-07-05 05:08:03 -0300

    refactor using prime class

--- 3.py
+++ 3.py
@@ -1,36 +1,5 @@
-from math import sqrt
+from lib.sieve import Factor
 
 N = 600851475143
-P = [2]
-S = set()
-
-def is_prime(p):
-    for n in range(2, int(sqrt(p))+1):
-        if p % n == 0:
-            return False
-    return True
-
-def next_prime(p):
-    p += 1 + (p%2)
-    while not is_prime(p):
-        p += 2
-    return p
-
-def lowest_factor(n):
-    global P
-    if n == 1:
-        AssertionError
-    for p in P:
-        if n % p == 0:
-            return p
-    q = next_prime(P[-1])
-    while n % q != 0:
-        P += [q]
-        q = next_prime(q)
-    return q
-    
-while N != 1:
-    factor = lowest_factor(N)
-    S.add(factor)
-    N //= factor
-print(max(S))
+factors = Factor(600851475143).factors()
+print(max(factors))
